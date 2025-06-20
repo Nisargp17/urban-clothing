0070ce1f5b5621aef0fab56754e004b105859438
Nisargp17 <nisargsudhir6028@gmail.com>
2025-06-20 15:02:30 +0530

    footer commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Comp2 from "./components/Comp2";
 import Comp3 from "./components/Comp3";
 import Comp4 from "./components/Comp4";
+import Comp5 from "./components/Comp5";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Shoes from "./components/shoes";
@@ -15,6 +16,7 @@ function App() {
       <Comp3 />
       <Comp4 />
       <Shoes />
+      <Comp5 />
     </>
   );
 }
--- src/components/Comp5.jsx
+++ src/components/Comp5.jsx
@@ -1,14 +1,46 @@
+import { useState, useEffect } from "react";
+import img1 from "/src/assets/11.svg";
+
 function Comp5() {
+  const [showFirst, setShowFirst] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShowFirst((prev) => !prev);
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <>
-      <section className=" h-[75vh] flex flex-col items-end justify-end mr-[10vw]">
-        <div className="font-[600] text-[13vw] leading-none">
-          Good things come to those who wait – Déplacé Maison is what has been
+    <section className="h-[65vh] flex items-center justify-end mr-[10vw] overflow-hidden">
+      <div className="w-[20vw]">
+        <img src={img1} alt="" />
+      </div>
+
+      <div className="w-[40vw] h-[20vh] flex items-center justify-end relative overflow-hidden">
+        <div
+          className={`absolute w-[35vw] transition-all duration-1500 ease-in-out ${
+            showFirst ? "translate-y-0" : "-translate-y-[10vw]"
+          } text-[1.5vw] font-[400] leading-none`}
+        >
+          Good things come to those who wait – Urban Clothing is what has been
           missing in the modern fashion industry for years. Buy a shoe of high
-          quality and design it finally happened.{" "}
+          quality and design it finally happened.
         </div>
-      </section>
-    </>
+
+        <div
+          className={`absolute w-[35vw] transition-all duration-1500 ease-in-out ${
+            !showFirst ? "translate-y-0" : "translate-y-[10vw]"
+          } text-[1.5vw] font-[400] leading-none`}
+        >
+          Urban Clothing is a convincer for anticipation. The urban trekking as
+          never seen before. An exceptional product that has no equal alongside
+          a great team represent the brand professionally.
+        </div>
+      </div>
+    </section>
   );
 }
+
 export default Comp5;
